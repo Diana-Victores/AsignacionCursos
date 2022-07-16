4469f2408f7f27cbc3450285169d101c558936cd
Diana Victores <dcpernillo@gmail.com>
2022-07-15 23:02:47 -0600

    ActualizacionPrototipo

--- CursosAsignacionProyecto1/ListadoAlumnosSeccion.Designer.cs
+++ CursosAsignacionProyecto1/ListadoAlumnosSeccion.Designer.cs
@@ -29,12 +29,154 @@ namespace CursosAsignacionProyecto1
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.btn_buscar = new System.Windows.Forms.Button();
+            this.txt_idseccion = new System.Windows.Forms.TextBox();
+            this.txt_idcurso = new System.Windows.Forms.TextBox();
+            this.lbl_idseccion = new System.Windows.Forms.Label();
+            this.lbl_idcurso = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.DataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_salir.Location = new System.Drawing.Point(500, 487);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(85, 38);
+            this.btn_salir.TabIndex = 13;
+            this.btn_salir.Text = "Salir";
+            this.btn_salir.UseVisualStyleBackColor = true;
+            // 
+            // btn_buscar
+            // 
+            this.btn_buscar.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_buscar.Location = new System.Drawing.Point(500, 51);
+            this.btn_buscar.Name = "btn_buscar";
+            this.btn_buscar.Size = new System.Drawing.Size(85, 38);
+            this.btn_buscar.TabIndex = 12;
+            this.btn_buscar.Text = "Buscar";
+            this.btn_buscar.UseVisualStyleBackColor = true;
+            // 
+            // txt_idseccion
+            // 
+            this.txt_idseccion.Location = new System.Drawing.Point(488, 10);
+            this.txt_idseccion.Name = "txt_idseccion";
+            this.txt_idseccion.Size = new System.Drawing.Size(97, 25);
+            this.txt_idseccion.TabIndex = 11;
+            // 
+            // txt_idcurso
+            // 
+            this.txt_idcurso.Location = new System.Drawing.Point(102, 17);
+            this.txt_idcurso.Name = "txt_idcurso";
+            this.txt_idcurso.Size = new System.Drawing.Size(97, 25);
+            this.txt_idcurso.TabIndex = 10;
+            // 
+            // lbl_idseccion
+            // 
+            this.lbl_idseccion.AutoSize = true;
+            this.lbl_idseccion.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_idseccion.Location = new System.Drawing.Point(368, 17);
+            this.lbl_idseccion.Name = "lbl_idseccion";
+            this.lbl_idseccion.Size = new System.Drawing.Size(76, 17);
+            this.lbl_idseccion.TabIndex = 9;
+            this.lbl_idseccion.Text = "Id Seccion";
+            // 
+            // lbl_idcurso
+            // 
+            this.lbl_idcurso.AutoSize = true;
+            this.lbl_idcurso.Font = new System.Drawing.Font("Arial", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_idcurso.Location = new System.Drawing.Point(16, 16);
+            this.lbl_idcurso.Name = "lbl_idcurso";
+            this.lbl_idcurso.Size = new System.Drawing.Size(63, 17);
+            this.lbl_idcurso.TabIndex = 8;
+            this.lbl_idcurso.Text = "Id Curso";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel2.Controls.Add(this.DataGridView1);
+            this.panel2.Controls.Add(this.btn_salir);
+            this.panel2.Controls.Add(this.btn_buscar);
+            this.panel2.Controls.Add(this.txt_idseccion);
+            this.panel2.Controls.Add(this.txt_idcurso);
+            this.panel2.Controls.Add(this.lbl_idseccion);
+            this.panel2.Controls.Add(this.lbl_idcurso);
+            this.panel2.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel2.Location = new System.Drawing.Point(12, 11);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(666, 544);
+            this.panel2.TabIndex = 15;
+            // 
+            // DataGridView1
+            // 
+            this.DataGridView1.AllowUserToAddRows = false;
+            this.DataGridView1.AllowUserToDeleteRows = false;
+            this.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.DataGridView1.BackgroundColor = System.Drawing.SystemColors.ControlLightLight;
+            this.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.DataGridView1.Location = new System.Drawing.Point(3, 137);
+            this.DataGridView1.Name = "DataGridView1";
+            this.DataGridView1.ReadOnly = true;
+            this.DataGridView1.RowHeadersVisible = false;
+            this.DataGridView1.Size = new System.Drawing.Size(651, 279);
+            this.DataGridView1.TabIndex = 14;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "No.";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Id Curso";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Id Seccion";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // ListadoAlumnosSeccion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form2";
+            this.ClientSize = new System.Drawing.Size(697, 559);
+            this.Controls.Add(this.panel2);
+            this.Name = "ListadoAlumnosSeccion";
+            this.Text = "Listado Alumnos Seccion";
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_salir;
+        private System.Windows.Forms.Button btn_buscar;
+        private System.Windows.Forms.TextBox txt_idseccion;
+        private System.Windows.Forms.TextBox txt_idcurso;
+        private System.Windows.Forms.Label lbl_idseccion;
+        private System.Windows.Forms.Label lbl_idcurso;
+        private System.Windows.Forms.Panel panel2;
+        internal System.Windows.Forms.DataGridView DataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
     }
 }
