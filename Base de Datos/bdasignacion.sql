9026e922d37494d75b98ea4a21d3b950694cc934
joselyneR <linriveracifu@hotmail.com>
2022-07-23 12:45:27 -0600

    SubiendomantenimientoyllenadodetablasJoselyne0901175

--- Base de Datos/bdasignacion.sql
+++ Base de Datos/bdasignacion.sql
@@ -16,6 +16,8 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+CREATE DATABASE AsignacionCurso2 CHARACTER SET utf8mb4;
+USE AsignacionCurso2;
 --
 -- Table structure for table `alumno`
 --
